244a432ea539739e5d2e70e15fed42bcef3e6594
aarondupon <info@aarondupon.be>
2019-02-18 16:57:34 +0100

    clean up

--- src/scripts/compose/createESCElement.ts
+++ src/scripts/compose/createESCElement.ts
@@ -1,6 +1,6 @@
 import compose from './compose';
 import { vec3, mat4 } from 'gl-matrix';
-import {_system_Behavior} from '../behaviors/ecs/_system_Behavior'
+import { _system_Behavior } from '../behaviors/ecs/_system_Behavior';
 
 declare interface IElement{
   uid: string;
@@ -14,7 +14,7 @@ declare interface IElement{
 const createElement = (...behaviors:{ (props:any): IElement; }[]) => (props = {}) => {
   // const element:IElement = compose(...behaviors,_system_Behavior({}))(props);
   const element:IElement = compose(...behaviors)(props);
-  const comp = Object.assign({uid:Date.now()}, element);
+  const comp = Object.assign({ uid:Date.now() }, element);
   return comp;
 };
 
